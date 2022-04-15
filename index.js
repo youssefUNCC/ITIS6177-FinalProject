0e32ea88c0f783c7d516e7b6346a9dfdbcdb6ddd
yshehata <yshehata@uncc.edu>
2022-04-15 18:03:20 -0400

    Project Complete - Implemented face detection and verification

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const express = require('express');
 const axios = require('axios').default;
 const app = express();
 const port = 3000;
-const subscriptionKey = 'SECRET_CODE'
-const endpoint = 'TOP_sECRET_END_POINT' + '/face/v1.0/detect'
+const subscriptionKey = 'TOP_SECRET_CODE';
+const detectEndpoint = 'https://itis6177-finalproject.cognitiveservices.azure.com/' + '/face/v1.0/detect';
+const verifyEndpoint = 'https://itis6177-finalproject.cognitiveservices.azure.com/' + '/face/v1.0/verify';
 
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req,res)=>{
-    res.send('POST a url image to the standard path "\/\" ');
+    res.send('Visit the path "\/detect\" or "\/verify\" for directions');
 });
 
-app.post('/', (req,res, next)=>{
+app.get('/detect', (req,res)=>{
+    res.send('POST a url image to the path "\/detect\" where the key is "\image\" and the value is "\{YOUR_IMAGE_URL}\" ');
+});
+
+app.get('/verify', (req,res)=>{
+    res.send('POST a faceID taken from the "\/detect\" endpoint, where the first key is "faceId1\" and the value is "\{YOUR_FACEID_FOR_FIRST_IMAGE}\" amd the second key is "faceId2\" and the second value is "\{YOUR_FACEID_FOR_SECOND_IMAGE}\" ');
+});
+
+app.post('/detect', (req,res, next)=>{
 
 
 let imageUrl = req.body.image;
 
 axios({
     method: 'post',
-    url: endpoint,
+    url: detectEndpoint,
     params : {
 		detectionModel: 'detection_01',
         returnFaceId: true,
@@ -32,13 +41,45 @@ axios({
     console.log('Status text: ' + response.status);
     console.log('Status text: ' + response.statusText);
     console.log();
-    console.log(response.data);
-    res.json(response.data);
+    console.log(JSON.stringify(response.data));
+    console.log(response.data[0].faceAttributes.emotion);
+    console.log(response.data[0].faceId);
+    res.json(JSON.stringify(response.data));
 }).catch(function (error) {
     console.log(error)
     next();
 });
 });
+
+app.post('/verify', (req,res, next)=>{
+    
+    let face1 = req.body.faceId1;
+    let face2 = req.body.faceId2;
+
+    
+    
+    axios({
+        method: 'post',
+        url: verifyEndpoint,
+        params : {
+            
+        },
+        data: {
+            faceId1: face1,
+            faceId2: face2
+        },
+        headers: { 'Ocp-Apim-Subscription-Key': subscriptionKey }
+    }).then(function (response) {
+        console.log('Status text: ' + response.status);
+        console.log('Status text: ' + response.statusText);
+        console.log();
+        console.log(response.data);
+        res.json(response.data);
+    }).catch(function (error) {
+        console.log(error)
+        next();
+    });
+    });
 
 
 app.listen(port, ()=>{
